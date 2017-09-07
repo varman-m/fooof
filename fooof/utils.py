f213c5806ee3579f8bfd9354f14cb09bf7e3f369
Jenny Hamer <jhamer@ucsd.edu>
2017-09-07 16:12:16 -0700

    Added optional ignore_range parameter & corresponding logic to trim_psd (with minor modification to masking method)

--- fooof/utils.py
+++ fooof/utils.py
@@ -25,7 +25,7 @@ def group_three(vec):
     return [list(vec[i:i+3]) for i in range(0, len(vec), 3)]
 
 
-def trim_psd(freqs, psd, f_range):
+def trim_psd(freqs, psd, f_range, ignore_range=None):
     """Extract frequency range of interest from PSD data.
 
     Parameters
@@ -34,8 +34,10 @@ def trim_psd(freqs, psd, f_range):
         Frequency values for the PSD.
     psd : 1d array
         Power spectral density values.
-    f_range: list of [float, float]
+    f_range : list of [float, float]
         Frequency range to restrict to.
+    ignore_range : list of lists of [float, float], optional
+        Inclusive range(s) of input (frequencies and corresponding PSD values) to exclude from fitting.
 
     Returns
     -------
@@ -48,13 +50,20 @@ def trim_psd(freqs, psd, f_range):
     -----
     This function extracts frequency ranges >= f_low and <= f_high.
         - It does not round to below or above f_low and f_high, respectively.
+    It optionally excludes ranges within f_low and f_high to ignore during fitting
     """
 
-    # Create mask to index only requested frequencies
-    f_mask = np.logical_and(freqs >= f_range[0], freqs <= f_range[1])
+    # Create mask to index only requested frequency range(s)
+    f_mask = np.zeros_like(freqs)
+    f_mask[np.logical_and(freqs >= f_range[0], freqs <= f_range[1])] = 1
 
-    # Restrict freqs & psd to requested range
-    freqs_ext = freqs[f_mask]
-    psd_ext = psd[f_mask]
+    # If specified, ignore sub-range(s)
+    if ignore_range != None:
+        for ignore in ignore_range:
+            f_mask[np.logical_and(freqs >= ignore[0], freqs <= ignore[1])] = 0
+    
+    # Restrict freqs & psd values to the specified range(s)
+    freqs_ext = freqs[f_mask == 1]
+    psd_ext = psd[f_mask == 1]
 
     return freqs_ext, psd_ext
